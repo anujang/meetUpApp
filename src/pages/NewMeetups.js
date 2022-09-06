9b53c2163e9ee2a5943130bfd2ccce0a909ab28e
Anujan Ganeshalingam <anujan2000@live.co.uk>
2022-09-06 11:26:53 +0100

    updated to recieve data from our firebase database

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,6 +1,10 @@
+import {useHistory} from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-meetup-app-77789-default-rtdb.firebaseio.com//meetups.json",
@@ -11,7 +15,9 @@ function NewMeetupsPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      history.replace('/')
+    });
   }
   return (
     <section>
